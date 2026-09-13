71e8ea819a17e820dc8beda576702ffcaf96ccb4
ninjasaskeh <ariefmaizaki1@gmail.com>
2026-09-13 09:11:21 +0700

    last?

--- src/constants/index.js
+++ src/constants/index.js
@@ -248,7 +248,7 @@ export const SPOTIFY_DATA = {
       src: "/music/Niall Horan This Town Live.mp3",
     },
     {
-      title: "Osen Hutasoit",
+      title: "Hutasoit Saonari Sogot Haduan",
       artist: "Osen Hutasoit",
       album: "Osen Hutasoit",
       duration: "5:43",
